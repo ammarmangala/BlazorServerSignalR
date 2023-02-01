c5f18e04c8ddc73f4e9e251e6e7bcae3cd11d21d
ammarmangala <ammar.mangala@gmail.com>
2023-02-01 16:18:54 +0100

    namespaces for Microsoft.AspNetCore.ResponseCompression and the ChatHub class in program.cs file

--- BlazorServerSignalRApp/Program.cs
+++ BlazorServerSignalRApp/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BlazorServerSignalRApp.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServerSignalRApp.Server.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
